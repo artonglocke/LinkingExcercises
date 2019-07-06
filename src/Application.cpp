843911dcb97dba53b1d24221fb6bcf31de77a8ba
visualstudiomachina <visualstudiomachina@outlook.com>
2019-07-06 14:07:59 +0200

    Dynamic linking of glfw

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 #include <memory>
+#include "GLFW/glfw3.h"
 
-class Entity
-{
-public:
-	Entity();
-	~Entity();
-
-private:
-
-};
 int main()
 {
-	int* someNumber = (int*)malloc(sizeof(int));
+	int init = glfwInit();
 
-	free(someNumber);
-
-	int* newNumber = new int;
-
-	delete newNumber;
-
-	Entity* mallocEntity = (Entity*)malloc(sizeof(Entity));
-
-	free(mallocEntity);
-
-	Entity* newEntity = new Entity();
-
-	delete newEntity;
-
-	//Summary: Malloc ne poziva constructor klase, free ne poziva destructor klase
-	//Summary: new poziva constructor, delete poziva destructor
+	std::cin.get();
 }
